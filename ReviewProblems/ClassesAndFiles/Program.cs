b81f155a058e34e025a216195cba29810eeda3a2
Elise Nill <Elise.C.Nill-1@ou.edu>
2020-09-01 18:03:22 -0500

    completed student class

--- ReviewProblems/ClassesAndFiles/Program.cs
+++ ReviewProblems/ClassesAndFiles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassesAndFiles
 {
@@ -6,7 +7,28 @@ namespace ClassesAndFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> students = new List<Student>();
+            Student stud1 = new Student(); //constructor
+            stud1.name = "Micah";
+            stud1.id = 2;
+            students.Add(stud1);
+
+            
+            Student stud2 = new Student("Elise", 1);
+            students.Add(stud2);
+
+            foreach (var stud in students)
+            {
+                Console.WriteLine(stud);
+            }
+
+            DateTime current = DateTime.Now;
+            TimeSpan difference = current - stud1.enrollDate;
+            Console.WriteLine($"It has been {difference.TotalMilliseconds} since {stud1.name} has enrolled.");
+            
+            Console.WriteLine($"There are currently {Student.totalStudents} enrolled");
+
+
         }
     }
 }
---
+++ ReviewProblems/ClassesAndFiles/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndFiles
+{
+    class Student
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public DateTime enrollDate { get; set; }
+        public static int totalStudents { get; set; }
+
+      
+        public Student (string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+            enrollDate = DateTime.Now;
+            totalStudents++;
+        }
+        
+        public Student()
+        {
+            //set the default values
+            name = string.Empty;
+            id = -1;
+            enrollDate = DateTime.Now;
+            totalStudents++;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} has an id of {id}"; 
+        }
+        // private string getEmailAddress()
+        // {
+        //return 0;
+        // }
+        public int getTotalStudents()
+        {
+            return totalStudents;
+        }
+
+    }
+}
