c0e80ff5fab78e032e01a68f9e6014319516fcbc
Elise Nill <Elise.C.Nill-1@ou.edu>
2020-09-03 18:09:14 -0500

    finished student class

--- ReviewProblems/MoreDifficultStudentExample/Program.cs
+++ ReviewProblems/MoreDifficultStudentExample/Program.cs
@@ -6,7 +6,30 @@ namespace MoreDifficultStudentExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("enter a number.");
+            double x = Convert.ToDouble(Console.ReadLine());
+            //create an instance of the class student
+            Student myStudent = new Student(5,"Elise", "Nill", 100000);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+
+            try
+            {
+                myStudent.MakePayment(-500);
+                Console.WriteLine(myStudent);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Console.WriteLine(myStudent);
+
         }
     }
 }
---
+++ ReviewProblems/MoreDifficultStudentExample/Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreDifficultStudentExample
+{
+    class Student
+    {
+        //if it is private you do not need to do the get and set
+        public int SoonerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOnProbation { get; set; }
+        public double GPA { get; set; }
+        private double BursarBalance;
+
+        //ctor tab tab and it will fill in the constructor for you
+        public Student()
+        {
+            SoonerID = -1;
+            FirstName = string.Empty;
+            LastName = "";
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 10000;
+        }
+
+        public Student(int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerID = -id;
+            FirstName = fName;
+            LastName = lName;
+            //reason we have to put the two below in both methods is because they are methods
+            IsOnProbation = false;
+            GPA = 0;
+            this.BursarBalance = BursarBalance;
+            //could also just change one of the bursarBalance names
+        }
+
+        public void MakePayment (double amount)
+        {
+            if(amount > 0)
+            {
+                BursarBalance = BursarBalance - amount;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+
+        public override string ToString()
+        {
+            string result = $"{FirstName} {LastName} ({SoonerID}) has a {GPA.ToString("N2")} and owes {BursarBalance}";
+            
+
+            if(IsOnProbation == true)
+            {
+                result += "\nIs on Probation.";
+            }
+            else
+            {
+                result += "\nIs not on Probation.";
+            }
+            return result;
+        }
+    }
+}
