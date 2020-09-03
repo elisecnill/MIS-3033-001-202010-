ceb5046eb080383e702e170d52f994531ad4b7f8
Elise Nill <Elise.C.Nill-1@ou.edu>
2020-09-03 17:16:32 -0500

    file read set up

---
+++ ReviewProblems/ReadingFileExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReadingFileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\Users\elise\Downloads\Pride and Prejudice by Jane Austen(1).txt";
+            Console.WriteLine("Hello\nWorld");
+
+        }
+    }
+}
